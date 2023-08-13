d151b12eda807289a5b8f75b65bde0ce2f0af936
akambarish <64399483+akambarish@users.noreply.github.com>
2023-08-13 11:55:32 +0530

    Add files via upload

---
+++ Streamlit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# import module
+import streamlit as st
+ 
+# Title
+st.title("Hello GeeksForGeeks !!!")
+
+
+# In[3]:
+
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+
+# In[ ]:
+
+
+
+
